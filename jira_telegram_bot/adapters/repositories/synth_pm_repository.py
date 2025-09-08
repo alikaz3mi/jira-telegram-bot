3b71fe6284ff86871789e37f67cf504eb02b367d
alikazemi <alikaz3mi1374@gmail.com>
2025-09-08 11:59:14 +0330

    chore: updated synth pm board

--- jira_telegram_bot/adapters/repositories/synth_pm_repository.py
+++ jira_telegram_bot/adapters/repositories/synth_pm_repository.py
@@ -76,7 +76,7 @@ class SynthPMRepository(SynthPMRepositoryInterface):
             # TODO: get the range of headers dynamically.
             values = await self.google_sheet_client.get_values(
                 self.settings.google_sheets_id,
-                f"{self.settings.developer_board_worksheet_name}!A:AR",
+                f"{self.settings.developer_board_worksheet_name}!A:AT",
             )
 
             if not values or len(values) < 2:
