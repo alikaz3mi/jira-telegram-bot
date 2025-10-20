087d75e5df577271a4a0ddb1f2d129976cf18f14
alikazemi <alikaz3mi1374@gmail.com>
2025-10-20 14:53:17 +0330

    fix: update worksheet range and refine feature filtering logic in SynthPM use case

--- jira_telegram_bot/adapters/repositories/synth_pm_repository.py
+++ jira_telegram_bot/adapters/repositories/synth_pm_repository.py
@@ -84,7 +84,7 @@ class SynthPMRepository(SynthPMRepositoryInterface):
             # TODO: get the range of headers dynamically.
             values = await self.google_sheet_client.get_values(
                 self.settings.google_sheets_id,
-                f"{self.settings.developer_board_worksheet_name}!A:AT",
+                f"{self.settings.developer_board_worksheet_name}!A:AU",
             )
 
             if not values or len(values) < 2:
